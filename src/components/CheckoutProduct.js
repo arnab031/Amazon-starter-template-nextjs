0c856e849cf870009f4bfab45c9bdae3b13b8847
arnab031 <51916075+arnab031@users.noreply.github.com>
2021-07-07 18:54:27 +0530

    Added checkout & Google Auth

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
+import { addToBasket, removeFromBasket } from "../slices/basketSlice";
+import { useDispatch } from "react-redux";
 
 function CheckoutProduct({
   id,
@@ -13,7 +15,27 @@ function CheckoutProduct({
   image,
   hasPrime,
 }) {
-  //console.log(rating);
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      rating,
+      description,
+      category,
+      image,
+      hasPrime,
+    };
+    //Push item into redux store
+    dispatch(addToBasket(product));
+  };
+
+  const removeItemFromBasket = () => {
+    //Remove item from basket
+    dispatch(removeFromBasket({id}));
+  };
   return (
     <div className="grid grid-cols-5">
       <Image src={image} height={200} width={200} objectFit="contain" />
@@ -28,7 +50,7 @@ function CheckoutProduct({
               <StarIcon key={i} className="h-5 text-yellow-500" />
             ))}
         </div>
-        <p className="text-xs mt-2 mb-2 line-clamp-3">{description}</p>
+        <p className="text-xs m-2 line-clamp-3">{description}</p>
         <Currency quantity={price} currency="INR" />
 
         {hasPrime && (
@@ -47,8 +69,12 @@ function CheckoutProduct({
       </div>
       {/* Right and remove buttons */}
       <div className="flex flex-col space-y-2 my-auto justify-self-end">
-        <button className="button">Add to Basket</button>
-        <button className="button">Remove from Basket</button>
+        <button className="button" onClick={addItemToBasket}>
+          Add to Basket
+        </button>
+        <button className="button" onClick={removeItemFromBasket}>
+          Remove from Basket
+        </button>
       </div>
     </div>
   );
