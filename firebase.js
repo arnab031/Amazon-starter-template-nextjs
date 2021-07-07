def7acb33dc19886a1adf73f10048b3ce8c67a68
arnab031 <51916075+arnab031@users.noreply.github.com>
2021-07-08 02:20:07 +0530

    Stripe Checkout & Webhooks

--- firebase.js
+++ firebase.js
@@ -1,10 +1,18 @@
-import firebase from firebase;
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB9IHocBS0wNCOXEx5pm8EHJ9IPyuZdHxA",
-    authDomain: "clone-d4ef7.firebaseapp.com",
-    projectId: "clone-d4ef7",
-    storageBucket: "clone-d4ef7.appspot.com",
-    messagingSenderId: "729124010096",
-    appId: "1:729124010096:web:341282fd1dc4a2db401cb5"
-  };
+  apiKey: "AIzaSyB9IHocBS0wNCOXEx5pm8EHJ9IPyuZdHxA",
+  authDomain: "clone-d4ef7.firebaseapp.com",
+  projectId: "clone-d4ef7",
+  storageBucket: "clone-d4ef7.appspot.com",
+  messagingSenderId: "729124010096",
+  appId: "1:729124010096:web:341282fd1dc4a2db401cb5",
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export default db;
