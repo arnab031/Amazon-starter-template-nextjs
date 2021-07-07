299540d1f6058b6e4d3c0562d1facecbbf04830b
arnab031 <51916075+arnab031@users.noreply.github.com>
2021-07-07 06:41:24 +0530

    Add Busket & Auth Features add

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from firebase;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB9IHocBS0wNCOXEx5pm8EHJ9IPyuZdHxA",
+    authDomain: "clone-d4ef7.firebaseapp.com",
+    projectId: "clone-d4ef7",
+    storageBucket: "clone-d4ef7.appspot.com",
+    messagingSenderId: "729124010096",
+    appId: "1:729124010096:web:341282fd1dc4a2db401cb5"
+  };
